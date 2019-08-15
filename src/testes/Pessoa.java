5f9ad9b5b6c2147c2b1f58bfdffc366fbaa9d998
yuris <yuris@Yuri-Pc>
2019-08-15 15:58:49 -0300

    Transf. entre contas com validações

---
+++ src/testes/Pessoa.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+/**
+ *
+ * @author yuris
+ */
+public class Pessoa {
+    
+    private String nome;
+    private double saldo;
+    private double debito;
+    private double recebeu;
+
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getDebito() {
+        return debito;
+    }
+
+    public void setDebito(double debito) {
+        this.debito = debito;
+    }
+
+    public double getRecebeu() {
+        return recebeu;
+    }
+
+    public void setRecebeu(double recebeu) {
+        this.recebeu = recebeu;
+    }
+    
+    
+    public String toString(){
+        return "Nome: " + this.nome + "\nSaldo: " + this.saldo + "\nDepositou: " + this.debito + "\nRecebeu: " + this.recebeu + "\n-------------------";
+    }
+}
---
+++ src/testes/Testes.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+/**
+ *
+ * @author yuris
+ */
+public class Testes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Pessoa pessoa1 = new Pessoa();
+         Pessoa pessoa2 = new Pessoa();
+         
+         
+         pessoa1.setNome("Yuri");
+         pessoa1.setSaldo(1000);
+         pessoa1.setDebito(200);
+         
+         pessoa2.setNome("Cris");
+         pessoa2.setSaldo(1000);
+         pessoa2.setDebito(0);
+         
+         if(pessoa1.getDebito() > 0 && pessoa2.getDebito() > 0){
+             System.out.println("VOCÊ ESTÁ TENTANDO TRANSFERIR PARA DUAS CONTAS AO MESMO TEMPO");
+         } else {
+             if(pessoa2.getSaldo() >= pessoa2.getDebito() && pessoa2.getDebito() > 0){
+               //depositando para o Yuri
+               pessoa1.setSaldo(pessoa1.getSaldo() + pessoa2.getDebito());//DEPOSITANDO
+               pessoa1.setRecebeu(pessoa2.getDebito());//DIZENDO QUANDO RECEBEU
+               pessoa2.setSaldo(pessoa2.getSaldo() - pessoa2.getDebito());//TIRANDO O VALOR DA OUTRA PESSOA
+               System.out.println(pessoa1);
+               System.out.println(pessoa2);
+            } else if(pessoa2.getDebito() > pessoa2.getSaldo()){
+                System.out.println("VALOR MUITO ALTO");
+            }
+
+            if(pessoa1.getSaldo() >= pessoa1.getDebito()&& pessoa1.getDebito() > 0){
+               //DEPOSITANDO PARA A CRIS
+               pessoa2.setSaldo(pessoa2.getSaldo() + pessoa1.getDebito());//DEPOSITANDO
+               pessoa2.setRecebeu(pessoa1.getDebito());//DIZENDO QUANDO RECEBEU
+               pessoa1.setSaldo(pessoa1.getSaldo() - pessoa1.getDebito());//TIRANDO O VALOR DA OUTRA PESSOA
+               System.out.println(pessoa1);
+               System.out.println(pessoa2);
+            } else if(pessoa1.getDebito() > pessoa1.getSaldo()){
+                System.out.println("VALOR MUITO ALTO");
+            }
+         }
+    }   
+}
